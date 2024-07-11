1b3512a0b76cfb2d1464190e31347c802d01073a
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 14:56:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+// Write your solution in this file!
+const employee = { name: "Sam" };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, ["streetAddress"]: "11 Broadway" };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee['streetAddress'] = '12 Broadway';
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+const destructivelyDeleteFromEmployeeByKey = (employee,key) => {
+  delete employee[key]
+  return employee
+}
